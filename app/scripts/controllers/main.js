47764df71db4b390fb7d98c1136f2038424e2acb
Leonardo <leonardo123k@gmail.com>
2020-07-11 18:09:31 -0300

    initial setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testeApp
+ */
+angular.module('testeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
